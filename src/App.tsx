a7e60e611fb1b60c7f49131f307ec9fb85920565
Gustavo Resende <gustavo@resende.app>
2021-06-25 17:47:30 -0300

    Aplicação funcional
    Commit com o conteúdo de todas as aulas do NLW

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { createContext } from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
